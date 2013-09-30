487b6f4839fb4b53f08ed967eabee0ddcf5a9a7d
Khash Sajadi <khash@sajadi.co.uk>
2013-09-30 14:19:59 +0100

    sorting of categories

--- _plugins/sorted_categories_plugin.rb
+++ _plugins/sorted_categories_plugin.rb
@@ -2,13 +2,28 @@
 module Jekyll
   class SortedCategoriesBuilder < Generator
   
+		ORDER = [ 
+			'getting-started', 
+			'stacks', 
+			'how-to', 
+			'stack-features',
+			'add-ins',
+			'api',
+			'cloud-providers',
+			'troubleshooting',
+			'web-server',
+			'your-account',
+			'jekyll'
+		]
+		
     safe true
     priority :high
 
     def generate(site)
-      site.config['sorted_categories'] = site.categories.map { |cat, posts|
-        [ cat, posts.size, posts ] }.sort { |a,b| b[1] <=> a[1] }
-    end
+			# sort all existing cats
+			site.config['sorted_categories'] = site.categories.map { |cat, posts| 
+				[cat, posts.size, posts] }.sort { |a,b| ORDER.index(a[0]) <=> ORDER.index(b[0]) }
+		end
 
   end
 end
