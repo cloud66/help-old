c194b809f71457513c6b3248e5628d029c76cbcf
Khash Sajadi <khash@sajadi.co.uk>
2013-10-02 11:33:44 +0100

    Remove the need to have index tank URL

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -6,9 +6,6 @@ module Jekyll
 		
     def generate(site)
       site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
-			
-			raise "Environment INDEXTANK_URL not found" unless ENV['INDEXTANK_URL']
-			
 			site.config['indextank_api_url'] = ENV['INDEXTANK_URL']
 			site.config['indextank_index'] = ENV['JEKYLL_ENV'] == 'production' ? 'help_pages' : 'dev_help_pages'
 			site.config['reindex'] = true if ENV['JEKYLL_REINDEX']
