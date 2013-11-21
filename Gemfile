52d220c70d772c904451574ee944841e1401a8cb
Khash Sajadi <khash@sajadi.co.uk>
2013-11-21 13:31:14 +0000

    Replcate IndexTank with ElasticSearch

--- Gemfile
+++ Gemfile
@@ -2,6 +2,6 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'jekyll-sass'
-gem 'indextank'
 gem 'nokogiri'
 gem 's3_website'
+gem 'elasticsearch'
--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -6,8 +6,8 @@ module Jekyll
 		
     def generate(site)
       site.config['env'] = ENV['JEKYLL_ENV'] || 'development'
-			site.config['indextank_api_url'] = ENV['INDEXTANK_URL']
-			site.config['indextank_index'] = ENV['JEKYLL_ENV'] == 'production' ? 'help_pages' : 'dev_help_pages'
+			site.config['elasticsearch_url'] = ENV['ELASTICSEARCH_URL'] || 'localhost:9200'
+			site.config['elasticsearch_index'] = ENV['JEKYLL_ENV'] == 'production' ? 'help_pages' : '	'
 			site.config['reindex'] = true if ENV['JEKYLL_REINDEX']
     end
  
